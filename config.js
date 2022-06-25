dff1e59cb5cfdbe8b638da8dfa5f64fb406124ba
Vaibhav <vaibhavsharma.ei@gmail.com>
2022-06-25 22:05:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 /*jslint node: true */
 
 module.exports = {
-    "adminPass": "super_password",
-    "serverURL": "http://localhost:8091/",
-    "serverDomain": "localhost",
+    "adminPass": "b43e14f803de359d978600e21ae1d90a16c2e9bfe628d0e4b597be72a84f8217",
+    "serverURL": "https://ec2-44-206-89-185.compute-1.amazonaws.com:5432",
+    "serverDomain": "ec2-44-206-89-185.compute-1.amazonaws.com",
     "environment": "development",
     "debug": true,
     "dbDebug": true,
